2a7df6111a4b5cac6fdb106ca099702f1a87816c
Alexander Miesen <alexmiesen@hotmail.com>
2020-10-24 22:46:06 +1100

    added species and gender validations

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,5 @@
 class Person < ApplicationRecord
-    validates :first_name, presence:true
-     
+    validates :first_name, presence: true
+    validates :species, presence: true
+    validates :gender, presence: true
 end
